97d94689cdacb741b8aa18f20a7cb810ca338b57
weeweeshka <tvbkmuthfcbvjd2008@gmail.com>
2025-08-16 19:48:53 +0300

    init

---
+++ internal/grpcClients/clients.go
@@ -0,0 +1,18 @@
+package grpcClients
+
+import (
+	pbSSO "github.com/weeweeshka/sso_proto/gen/go/sso"
+	pbTataisk "github.com/weeweeshka/tataisk_proto/gen/go/tataisk"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func RunGateway() (pbSSO.SsoClient, pbTataisk.TataiskClient) {
+	ssoConn, _ := grpc.NewClient("localhost:5001", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	tataiskConn, _ := grpc.NewClient("localhost:5000", grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	ssoClient := pbSSO.NewSsoClient(ssoConn)
+	tataiskClient := pbTataisk.NewTataiskClient(tataiskConn)
+
+	return ssoClient, tataiskClient
+}
